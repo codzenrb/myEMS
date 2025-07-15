6bccd56243aa228a8d04fb2b176f041fc21fcae9
Rishi Baagora <codzenrb@gmail.com>
2025-07-16 01:23:25 +0530

    Update Login.jsx

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -16,6 +16,7 @@ const Login = ({handleLogin}) => {
         
         <>
             <div className="w-full h-screen flex justify-center items-center p-4">
+                <h1 classname="text-center">Demo only — no real data is stored.</h1>
                 <div className="loginCard min-h-[400px] w-full max-w-md bg-zinc-800 rounded-xl hover:bg-zinc-900 shadow-lg">
 
                     <form 
